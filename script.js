7ef54d9d683ed510c5398eb09eb3b9f62dff4b10
mackv42 <mackv42@gmail.com>
2020-11-21 21:23:35 -0600

    IsPossible, Possible, makeMove functions finished

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ var canvas = document.getElementById("ChessBoard");
 var context = canvas.getContext("2d");
 
 function Clear(board){
-
+	let squareWidth = canvas.width / 8;
+	for(let i=0; i<8; i++){
+		for(let j=0; j<8; j++){
+			color = ((i+j+1)%2 == 0) ? "grey":"black";
+			context.fillStyle = color;
+			context.fillRect(i*squareWidth , j*squareWidth, squareWidth, squareWidth);
+		}
+	}
 }
+
+Clear();
 
 function RenderBoard(board){
 	//Clear(board);
@@ -14,15 +23,9 @@ function RenderBoard(board){
 }
 
 
-
-
 //List of moves made
 var PastMoves = [];
 
-
-function Check(){
-
-}
 
 const Pieces =  {
 	"King": {
@@ -66,7 +69,8 @@ const startingBoard = {
 		   {"name":"Pa", "x": 0, "y": 1}, {"name":"Pa", "x": 1, "y": 1}, {"name":"Pa", "x": 2, "y": 1}, {"name":"Pa", "x": 3, "y": 1}, {"name":"Pa", "x": 4, "y": 1}, {"name":"Pa", "x": 5, "y": 1}, {"name":"Pa", "x": 6, "y": 1}, {"name":"Pa", "x": 7, "y": 1}],
 
 	"black": [{"name":"Ro", "x": 0, "y": 7}, {"name":"Kn", "x": 1, "y": 7}, {"name":"Bi", "x": 2, "y": 7}, {"name":"Qu", "x": 3, "y": 7}, {"name":"Ki", "x": 4, "y": 7}, {"name":"Bi", "x": 5, "y": 7}, {"name":"Kn", "x": 6, "y": 7}, {"name":"Ro", "x": 7, "y": 7},
-		   {"name":"Pa", "x": 0, "y": 6}, {"name":"Pa", "x": 1, "y": 6}, {"name":"Pa", "x": 2, "y": 6}, {"name":"Pa", "x": 3, "y": 6}, {"name":"Pa", x: 4, "y": 6}, {"name":"Pa", "x": 5, "y": 6}, {"name":"Pa", "x": 6, "y": 6}, {"name":"Pa", "x": 7, "y": 6}]
+		   {"name":"Pa", "x": 0, "y": 6}, {"name":"Pa", "x": 1, "y": 6}, {"name":"Pa", "x": 2, "y": 6}, {"name":"Pa", "x": 3, "y": 6}, {"name":"Pa", x: 4, "y": 6}, {"name":"Pa", "x": 5, "y": 6}, {"name":"Pa", "x": 6, "y": 6}, {"name":"Pa", "x": 7, "y": 6}],
+	"turn": "white"	   
 }
 
 
@@ -78,23 +82,162 @@ var Move = function(p1, p2){
 }
 
 
-function getPiece(px, py){
-	let piece = startingBoard.white.filter( x => x.x == px && x.y == py)[0];
+function getPiece(board, px, py){
+	let color = "white";
+	let piece = board.white.filter( x => x.x == px && x.y == py)[0];
 	if(piece === undefined){
-		piece = startingBoard.black.filter( x => x.x == px && x.y == py)[0];
+		piece = board.black.filter( x => x.x == px && x.y == py)[0];
+		color = "black";
 	}
-	return piece;
+	if(piece === undefined){
+		return undefined;
+	}
+	return {"piece": piece, "color": color }
 }
 
-function Possible(board, move){
-	let SelectedPiece = getPiece(move.p1.x, move.p1.y);
-	if(SelectedPiece !== null){ return true;}
-	//can this piece do that?
+function inBounds(x, y){
+	if(x>0 && x<7 && y>0 && y<7){
+		return true;
+	}
 
+	return false;
+}
+
+function possibleMoves(board, pieceInfo){
+	let possibilities = [];
+	let piece = pieceInfo.piece;
+	let pieceType = PieceNames[piece.name];
+	
+	if(piece.name == "Pa"){
+		//in front of
+		//left diagonal
+		//right diagonal
+		if(pieceInfo.color == "white"){
+			if(getPiece(board, piece.x, piece.y+1) === undefined){ possibilities.push({"x": piece.x, "y": piece.y+1}); }
+			if(getPiece(board, piece.x+1, piece.y+1) !== undefined){ possibilities.push({"x": piece.x+1, "y": piece.y+1}); }
+			if(getPiece(board, piece.x-1, piece.y+1) !== undefined){ possibilities.push({"x": piece.x-1, "y": piece.y+1}); }
+		} else if(pieceInfo.color == "black"){
+			if(getPiece(board, piece.x, piece.y-1) === undefined){ possibilities.push({"x": piece.x, "y": piece.y-1}); }
+			if(getPiece(board, piece.x+1, piece.y-1) !== undefined){ possibilities.push({"x": piece.x+1, "y": piece.y-1}); }
+			if(getPiece(board, piece.x-1, piece.y-1) !== undefined){ possibilities.push({"x": piece.x-1, "y": piece.y-1}); }
+		}
+		return possibilities;
+	}
+
+	if(piece.name == "Kn"){
+		for(let i=0; i<pieceType.Moves.length; i++){
+			let x = piece.x + pieceType.Moves[i].x;
+			let y = piece.y + pieceType.Moves[i].y;
+			if(!inBounds(x, y)){ continue; }
+			if(getPiece(board, x, y) === undefined){ continue; }
+			possibilities.push({"x": x, "y": y});
+		}
+
+		return possibilities;
+	}
+
+	if(piece.name == "Ki"){
+		for(let i=0; i<piece.Moves.length; i++){
+			let x = piece.x+Moves[i].x;
+			let y = piece.y+Moves[i].y;
+			if(inBounds(x, y)){ possibilities.push({"x": x, "y": y});}
+		}
+	}
+
+	if(pieceType.Amount == 1){
+		for(let i=0; i<pieceType.Moves.length; i++){
+			let x = piece.x;
+			let y = piece.y;
+
+			while(inBounds(x, y)){
+				x+=pieceType.Moves[i].x;
+				y+=pieceType.Moves[i].y;
+				if(getPiece(board, x, y) !== undefined){
+					break;
+				}
+				possibilities.push({"x": x, "y": y});
+			}
+			
+		}
+	}
+	return possibilities;
+}
+
+//tells if white or black is in check
+function InCheck(board){
+	let blackKing = board["black"].filter(x => x.name == "Ki")[0];
+	let whiteKing = board["white"].filter(x => x.name == "Ki")[0];
+	for(let i=0; i<board["white"].length; i++){
+		let piece = board["white"][i];
+		let moves = possibleMoves(board, piece);
+
+		for(let j=0; j<moves.length; j++){
+			if(moves[j].x == blackKing.x && moves[j].y == blackKing.y){
+				return "black";
+			}
+		}
+	}
+
+	for(let i=0; i<board["black"].length; i++){
+		let piece = board["black"][i];
+		let moves = possibleMoves(board, piece);
+
+		for(let j=0; j<moves.length; j++){
+			if(moves[j].x == whiteKing.x && moves[j].y == whiteKing.y){
+				return "white";
+			}
+		}
+	}
+
+
+	return undefined;
+}
+
+// usage Possible(ChessBoard, Move{x1, y1, x2, y2})
+function Possible(board, move){
+	let SelectedPiece = getPiece(board, move.p1.x, move.p1.y);
+	if(SelectedPiece === null){ return false; }
+	if(move.p2.x > 7 || move.p2.x < 0 || move.p2.y >7 || move.p2.y < 0){ return false; }
+	if(move.p1.x > 7 || move.p1.x < 0 || move.p1.y > 7 || move.p2.y < 0){ return false; }
+	
+	let PossibleMoves = possibleMoves(board, SelectedPiece);
+	//can this piece do that?
+	for(let i=0; i<PossibleMoves.length; i++){
+		if(PossibleMoves[i].x == move.p2.x && PossibleMoves[i].y == move.p2.y){
+			return true;
+		}
+	}
 	//are you in check after a move? false
 	return false;
 }
 
-function MakeMove(){
+function copyBoard(board){
+	let newBoard = {};
+	newBoard["white"] = [];
+	newBoard["black"] = [];
+	for(let i=0; i<board.white.length; i++){
+		newBoard["white"].push({"name": board.white[i].name, "x": board.white[i].x, "y": board.white[i].y});
+	}
+
+	for(let i=0; i<board.black.length; i++){
+		newBoard["black"].push({"name": board.black[i].name, "x": board.black[i].x, "y": board.black[i].y});
+	}
+
+	newBoard.turn = board.turn;
+	return newBoard;
+}
+
+
+function MakeMove(board, x1, y1, x2, y2){
 // if(!Possible()){ return board; }
+	let newBoard = copyBoard(board);
+	pieceInfo = getPiece(newBoard, x1, y1);
+	if(board.turn == "white" && pieceInfo.color == "black"){ return undefined; }
+	if(board.turn == "black" && pieceInfo.color == "white"){ return undefined; }
+	if(Possible(board, {"p1": {"x": x1, "y": y1}, "p2": {"x": x2, "y": y2}})){
+		pieceInfo.piece.x = x2;
+		pieceInfo.piece.y = y2;
+		newBoard.turn = (newBoard.turn == "white") ? "black" : "white"; 
+		return newBoard;
+	}
 }
