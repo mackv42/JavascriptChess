2c4005525df6c06a398003cedcf494211a964953
mackv42 <mackv42@gmail.com>
2021-04-19 04:47:39 -0500

    add dockerfile!

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 
 const chess = require('./chess.js');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const bodyParser = require('body-parser');
 const users = require('./routes/signin');
